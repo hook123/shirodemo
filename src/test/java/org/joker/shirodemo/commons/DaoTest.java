d5728d36ad9cc4a6594c8d81df34eee50d90f168
joker <joker@cx-blog.com>
2019-03-24 12:46:47 +0800

    ignore修改和test提交

---
+++ src/test/java/org/joker/shirodemo/commons/DaoTest.java
@@ -0,0 +1,20 @@
+package org.joker.shirodemo.commons;
+
+import org.joker.shirodemo.common.model.UPermission;
+import org.junit.Test;
+
+/**
+ * @Author joker
+ * @Date 3/22/19 5:12 PM
+ * @Description
+ */
+public class DaoTest {
+    @Test
+    public void  toStringTest(){
+        UPermission uPermission = new UPermission();
+        uPermission.setId(123L);
+        uPermission.setUrl("http://www.baidu.com");
+        uPermission.setName("李自强");
+        System.out.println(uPermission.toString());
+    }
+}
